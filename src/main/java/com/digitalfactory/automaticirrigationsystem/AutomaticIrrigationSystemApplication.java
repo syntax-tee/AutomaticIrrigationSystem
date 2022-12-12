7587cdb222871310e25af15fbaac656cbcad4109
Taiye Emmanuel Ogunlade <ogunladetaiye@gmail.com>
2022-12-12 16:30:13 +0100

    Project setup

---
+++ src/main/java/com/digitalfactory/automaticirrigationsystem/AutomaticIrrigationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.digitalfactory.automaticirrigationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticIrrigationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomaticIrrigationSystemApplication.class, args);
+    }
+
+}
