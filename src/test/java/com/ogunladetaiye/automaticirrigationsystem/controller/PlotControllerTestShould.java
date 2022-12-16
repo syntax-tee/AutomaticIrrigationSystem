f85ed678df765d03b7036171f9ee3fee03f27884
Taiye Emmanuel Ogunlade <ogunladetaiye@gmail.com>
2022-12-16 12:47:40 +0100

    added Controller test for API endpoints using RESTAssured

--- src/test/java/com/ogunladetaiye/automaticirrigationsystem/controller/PlotControllerTestShould.java
+++ src/test/java/com/ogunladetaiye/automaticirrigationsystem/controller/PlotControllerTestShould.java
@@ -1,16 +1,58 @@
 package com.ogunladetaiye.automaticirrigationsystem.controller;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import io.restassured.RestAssured;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@ExtendWith(SpringExtension.class)
 class PlotControllerTestShould {
-       //TODO WRITE REST ASSURED TESTS
+
+    private Integer port = 8081;
+    private String CONTEXT_PATH = "/irrigation/";
+    private String API_PATH = "api/v1/plots/";
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    @Test
+    void getPlotByReturnSuccessfully() {
+        int id = 2;
+        var response =
+                given()
+                        .pathParam("id", id)
+                        .when()
+                        .get(CONTEXT_PATH + API_PATH + "{id}")
+                        .then()
+                        .assertThat()
+                        .statusCode(200)
+                        .body("id", equalTo(2))
+                        .body("length", equalTo(76.78F))
+                        .body("width", equalTo(25.94F))
+                        .body("plotName", equalTo("Plot 2"))
+                        .body("soilType", equalTo("CLAY"))
+                        .body("longitude", equalTo(34.5F))
+                        .body("latitude", equalTo(39.8F))
+                        .body("soilMoisture", equalTo(13.8F))
+                        .body("cropType", equalTo("vegetable"));
+    }
+
+    @Test
+    void getAllPlotSizeReturnSuccessfully() {
+        var response =
+                given().when()
+                        .get(CONTEXT_PATH + API_PATH).then()
+                        .statusCode(HttpStatus.OK.value());
+        Assertions.assertThat(response.body("size()", is(4)));
+    }
 
 
 }
